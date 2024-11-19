40ce6204af902a0e32df14c92986021becc5f428
KarenTriana <164826780+KarenTriana@users.noreply.github.com>
2024-11-19 16:02:27 -0500

    Delete backend/src directory

--- backend/src/main/java/com/kleanimals_backend/backend/repository/AnimalRepository.java
+++
@@ -1,7 +0,0 @@
-package com.kleanimals_backend.backend.repository;
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.kleanimals_backend.backend.models.Animales;
-
-public interface AnimalRepository extends JpaRepository<Animales, Long> {
-    
-}
--- backend/src/main/java/com/kleanimals_backend/backend/repository/PublicacionRepository.java
+++
@@ -1,9 +0,0 @@
-package com.kleanimals_backend.backend.repository;
-import java.util.List;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.kleanimals_backend.backend.models.Publicaciones;
-
-public interface PublicacionRepository extends JpaRepository<Publicaciones, Long> {
-    List<Publicaciones> findByTipoPublicacion(Publicaciones.Categoria tipoPublicacion);
-}
